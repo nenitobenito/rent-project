b800ccd4838b7cb298e291d64d4eb72cf7152a03
Benedikt Stranak <benedikt.stranak@gmail.com>
2026-08-14 17:53:39 +0100

    2026 AddressBase now loads London only

--- src/rent_project/load/addressbase.py
+++ src/rent_project/load/addressbase.py
@@ -5,6 +5,55 @@ from zipfile import ZipFile
 
 import pandas as pd
 
+# ----------------------------------------
+# Loading AddressBase Plus Schema
+# ----------------------------------------
+
+def load_schema_new(header_path):
+    """Parse a post-2016 (epoch 39+) header file nto column names.
+    Define dtypes schema (same for all csv tiles).
+    
+    Parameters
+    -----
+    header_path: a path to a csv file with column names.
+    """
+
+    columns = pd.read_csv(header_path,header=None,dtype=str).iloc[0].tolist()
+    INT_COLUMNS = ['UPRN','UDPRN','PARENT_UPRN','LOCAL_CUSTODIAN_CODE','BUILDING_NUMBER','SAO_START_NUMBER','SAO_END_NUMBER','PAO_START_NUMBER','PAO_END_NUMBER','USRN','OS_ADDRESS_TOID_VERSION','OS_ROADLINK_TOID_VERSION','OS_TOPO_TOID_VERSION','VOA_CT_RECORD','VOA_NDR_RECORD','MULTI_OCC_COUNT']
+    FLOAT_COLUMNS = ['X_COORDINATE', 'Y_COORDINATE', 'LATITUDE', 'LONGITUDE']
+    DATE_COLUMNS = ['STATE_DATE','LA_START_DATE','LAST_UPDATE_DATE','ENTRY_DATE','RM_START_DATE']
+    STRING_COLUMNS = [c for c in columns if c not in INT_COLUMNS + FLOAT_COLUMNS + DATE_COLUMNS]
+    DTYPES = {
+        **{c: "Int64" for c in INT_COLUMNS},
+        **{c: "float64" for c in FLOAT_COLUMNS},
+        **{c: "string" for c in STRING_COLUMNS},
+    }
+    return {"columns" : columns, "dtypes": DTYPES, "date_columns": DATE_COLUMNS}
+
+def load_schema_old(header_path):
+    """Parse a pre-2016 (epoch 38-) header file into column names.
+    Define dtypes schema (same for all csv tiles).
+    
+    Parameters
+    -----
+    header_path: a path to a csv file with column names.
+    """
+
+    columns = pd.read_csv(header_path,header=None,dtype=str).iloc[0].tolist()
+    INT_COLUMNS = ['UPRN','UDPRN','PARENT_UPRN','LOCAL_CUSTODIAN_CODE','BUILDING_NUMBER','SAO_START_NUMBER','SAO_END_NUMBER','PAO_START_NUMBER','PAO_END_NUMBER','USRN','OS_ADDRESS_TOID_VERSION','OS_ROADLINK_TOID_VERSION','OS_TOPO_TOID_VERSION','VOA_CT_RECORD','VOA_NDR_RECORD','MULTI_OCC_COUNT']
+    FLOAT_COLUMNS = ['X_COORDINATE', 'Y_COORDINATE', 'LATITUDE', 'LONGITUDE']
+    DATE_COLUMNS = ['STATE_DATE','LA_START_DATE','LAST_UPDATE_DATE','ENTRY_DATE','RM_START_DATE']
+    STRING_COLUMNS = [c for c in columns if c not in INT_COLUMNS + FLOAT_COLUMNS + DATE_COLUMNS]
+    DTYPES = {
+        **{c: "Int64" for c in INT_COLUMNS},
+        **{c: "float64" for c in FLOAT_COLUMNS},
+        **{c: "string" for c in STRING_COLUMNS},
+    }
+    return {"columns" : columns, "dtypes": DTYPES, "date_columns": DATE_COLUMNS}
+
+# ----------------------------------------
+# Loading AddressBase Plus 2026
+# ----------------------------------------
 
 def unzip_all(zip_dir, extract_dir):
     """Extract each .zip in zip_dir into extract_dir
@@ -27,29 +76,9 @@ def unzip_all(zip_dir, extract_dir):
         with ZipFile(zip_path) as zf:
             zf.extractall(target)
 
-def load_schema(header_path):
-    """Parse a tile header file into column names.
-    Define dtypes schema (same for all csv tiles).
-    
-    Parameters
-    -----
-    header_path: a path to a csv file with column names.
-    """
-
-    columns = pd.read_csv(header_path,header=None,dtype=str).iloc[0].tolist()
-    INT_COLUMNS = ['UPRN','UDPRN','PARENT_UPRN','LOCAL_CUSTODIAN_CODE','BUILDING_NUMBER','SAO_START_NUMBER','SAO_END_NUMBER','PAO_START_NUMBER','PAO_END_NUMBER','USRN','OS_ADDRESS_TOID_VERSION','OS_ROADLINK_TOID_VERSION','OS_TOPO_TOID_VERSION','VOA_CT_RECORD','VOA_NDR_RECORD','MULTI_OCC_COUNT']
-    FLOAT_COLUMNS = ['X_COORDINATE', 'Y_COORDINATE', 'LATITUDE', 'LONGITUDE']
-    DATE_COLUMNS = ['STATE_DATE','LA_START_DATE','LAST_UPDATE_DATE','ENTRY_DATE','RM_START_DATE']
-    STRING_COLUMNS = [c for c in columns if c not in INT_COLUMNS + FLOAT_COLUMNS + DATE_COLUMNS]
-    DTYPES = {
-        **{c: "Int64" for c in INT_COLUMNS},
-        **{c: "float64" for c in FLOAT_COLUMNS},
-        **{c: "string" for c in STRING_COLUMNS},
-    }
-    return {"columns" : columns, "dtypes": DTYPES, "date_columns": DATE_COLUMNS}
-
 def load_tile(schema, file_path):
-    """Load a single AddressBase Plus tile CSV
+    """Load a single AddressBase Plus tile CSV.
+    Drop addresses outside any of London's 33 local authorities.
     
     Parameters
     -----
@@ -57,14 +86,17 @@ def load_tile(schema, file_path):
     file_path: path to this specific tile's data CSV
     """
 
+    LONDON_BOROUGHS = ['CITY OF WESTMINSTER','TOWER HAMLETS','WANDSWORTH','CROYDON','BARNET','SOUTHWARK','LAMBETH','EALING','BROMLEY','CAMDEN','BRENT','LEWISHAM','NEWHAM','ENFIELD','GREENWICH','HACKNEY','ISLINGTON','HILLINGDON','HARINGEY','WALTHAM FOREST','HOUNSLOW','HAMMERSMITH AND FULHAM','REDBRIDGE','HAVERING','KENSINGTON AND CHELSEA','BEXLEY','MERTON','HARROW','RICHMOND UPON THAMES','BARKING AND DAGENHAM','SUTTON','KINGSTON UPON THAMES','CITY OF LONDON']
+
     df = pd.read_csv(
         file_path,header=None,
         names=schema["columns"],dtype=schema["dtypes"],
         parse_dates=schema["date_columns"]
     )
+    df = df[df['ADMINISTRATIVE_AREA'].isin(LONDON_BOROUGHS)]
     return df
 
-def load_and_append(header_path, extract_dir):
+def load_and_concatenate(header_path, extract_dir):
     """Load all CSVs and concatenate into a single DataFrame.
     
     Parameters
@@ -73,7 +105,7 @@ def load_and_append(header_path, extract_dir):
     extract_dir: path to a directory with CSV files to be loaded
     """
 
-    schema = load_schema(header_path)
+    schema = load_schema_new(header_path)
     csv_paths = sorted(Path(extract_dir).rglob("*.csv"))
     print(f"Loading {len(csv_paths)} tiles")
     tiles = [load_tile(schema,p) for p in csv_paths]
--- src/rent_project/pipelines/build_dataset.py
+++ src/rent_project/pipelines/build_dataset.py
@@ -1,10 +1,10 @@
 """Orchestration only"""
 
 from rent_project.config import ADDRESS_BASE_DIRECTORY
-from rent_project.load.addressbase import load_and_append, unzip_all
+from rent_project.load.addressbase import load_and_concatenate, unzip_all
 
 
-def step_build_addressbase():
+def step_build_addressbase2026():
     """Extracts, loads, concatenates, AddressBase Plus tiles (2026).
     Then writes csv.
     Does nothing if the combined CSV already exists.
@@ -26,7 +26,7 @@ def step_build_addressbase():
 
     # Load and concatenate all tiles (2026)
     print("Loading and concatenating tiles")
-    address_base_2026 = load_and_append(
+    address_base_2026 = load_and_concatenate(
         ADDRESS_BASE_DIRECTORY / "addressbase-plus-post-e-39-header.csv",
         ADDRESS_BASE_DIRECTORY / "2026" / "extracted"
     )
@@ -35,9 +35,15 @@ def step_build_addressbase():
     print("Writing 2026 AddressBase Plus (csv)")
     address_base_2026.to_csv(output_path,index=False)
 
+# def step_load_addressbase_all_years():
+#    """Loads AddressBase Plus 2011, 2021, 2026"""
 
 def main():
-    step_build_addressbase()
+    step_build_addressbase2026()
+    # step_load_addressbase_all_years
+    # step_subset_test_area()
+    # step_build_addressbase_history()
+    # step_filter_residential()
     # next step goes here
 
 if __name__ == "__main__":
